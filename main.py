99cb8a7cdbcd83f46083e4caf2a31330155e353c
HvozdikovVlad <vldgvozdikov228@gmail.com>
2023-02-05 14:23:33 +0100

    New version

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+class Student:
+    def __init__(self,height = 160):
+        self.height = height
+nick = Student()
+kate = Student(height=170)
+print(nick.height)
+print(kate.height)
+print("Hallo lieber Lehrer")
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
-def print_hi(name):
-
-    print(f'Hi, {name}')
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
